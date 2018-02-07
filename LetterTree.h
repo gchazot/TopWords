ccf0f0f4bc06e629783effe1ae2d229d903f79f1
Germain Chazot <g.chazot@gmail.com>
2018-02-07 02:17:57 +0000

    Add skeleton for LetterTree

---
+++ LetterTree.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "def.h"
+
+
+class LetterTree {
+public:
+	LetterTree(): _character('A' - 1), _count(0) {}
+
+	explicit LetterTree(CharType letter):
+		_character(letter),
+		_count(0) {
+	}
+
+	LetterTree * nextCharacter(CharType character){
+		return nullptr;
+	}
+
+	void increment() {
+	}
+
+private:
+	CharType _character;
+	size_t _count;
+	list<LetterTree> _next_letters;
+
+};
+
+class WordInserter {
+public:
+	explicit WordInserter(LetterTree * letterTree):
+		_currentLetter(letterTree) {
+	}
+
+	void append(CharType character) {
+		_currentLetter = _currentLetter->nextCharacter(character);
+	}
+
+	void incrementWord() {
+		_currentLetter->increment();
+	}
+
+private:
+	LetterTree * _currentLetter;
+};
--- def.h
+++ def.h
@@ -8,7 +8,6 @@ using namespace std;
 
 typedef std::string Word;
 typedef Word::value_type CharType;
-typedef std::map<Word, size_t> WordsCount;
 
 static const unsigned int ARCH_WORD_WIDTH_BITS = 64;
 static const unsigned int CHAR_WIDTH_BITS = 8 * sizeof(CharType);
